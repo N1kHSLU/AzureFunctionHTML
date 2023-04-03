b2b5113b50b3e2e604b64d6b3a88475bb453b820
V01D <V01DN1k@protonmail.com>
2023-04-03 22:11:33 +0200

    übung vom 1.4.2023 als func app implementiert

--- departure/__init__.py
+++ departure/__init__.py
@@ -1,12 +1,15 @@
 import logging
 
 import azure.functions as func
-
+import requests as http
+from datetime import datetime
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    f = req.params.get('f')
+    t = req.params.get('t')
     if not name:
         try:
             req_body = req.get_json()
@@ -15,10 +18,47 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if name and not t is None or not f is None:
+        url = f"http://transport.opendata.ch/v1/connections?from={f}&to={t}&limit=1"
+        respond = http.get(url)
+        data = respond.json()
+        for connections in data['connections']:
+            _from = connections['from']
+            departure = _from['departure']
+            date_obj = datetime.strptime(departure, "%Y-%m-%dT%H:%M:%S%z")
+            departure_fancied = date_obj.strftime("%d.%m.%Y %H:%M:%S")               
+
+        return func.HttpResponse(f"""
+        Hello, {name}. Das ist eine Azure Function App als Übung. Hier wurde die API Aufgabe vom 1.4.2023 als Function App implementiert.
+        Die Function App holt sich die nächste Abfahr der ÖV von f (from) nach t (to), wobei f & t anzugben sind.
+        Die nächste Abfahrt von {f} nach {t} ist exakt um: {departure_fancied}
+        """,
+        status_code=200)
+    if t is None or f is None:
+        return func.HttpResponse(f"""
+        Keine Parameter für Von und Nach angegeben. Bitte Paramter eingeben.
+        Beispiel:
+        ?name=nik&f=Rotkreuz&t=Luzern
+
+        name = <dein name>
+        f = from
+        t = to
+        """,
+        status_code=200)
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        url = f"http://transport.opendata.ch/v1/connections?from={f}&to={t}&limit=1"
+        respond = http.get(url)
+        data = respond.json()
+        for connections in data['connections']:
+            _from = connections['from']
+            departure = _from['departure']
+            date_obj = datetime.strptime(departure, "%Y-%m-%dT%H:%M:%S%z")
+            departure_fancied = date_obj.strftime("%d.%m.%Y %H:%M:%S")   
+
+        return func.HttpResponse(f"""
+        Das ist eine Azure Function App als Übung. Hier wurde die API Aufgabe vom 1.4.2023 als Function App implementiert.
+        Die Function App holt sich die nächste Abfahr der ÖV von f (from) nach t (to), webei f & t parameter sind.
+        Die nächste Abfahrt von {f} nach {t} ist exakt um: {departure_fancied}
+        """,
+        status_code=200)
+
